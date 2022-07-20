1faca492e8292ceb26c9d9f4e20636ab1817c423
HECHT Axel <axel.hct@gmail.com>
2022-07-20 14:23:21 +0200

    chore(icons): init edge-icnonify with heroicons and tabler

--- start/view.ts
+++ start/view.ts
@@ -7,3 +7,12 @@
 | boot.
 |
 */
+
+import View from '@ioc:Adonis/Core/View'
+import { addCollection, edgeIconify } from 'edge-iconify'
+import { icons as tablerIcons } from '@iconify-json/tabler'
+import { icons as heroIcons } from '@iconify-json/heroicons-outline'
+
+View.use(edgeIconify)
+addCollection(tablerIcons)
+addCollection(heroIcons)
