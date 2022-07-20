bdd2605c7c2eb7b2f4f35a4ace0c7b2980938f5e
HECHT Axel <axel.hct@gmail.com>
2022-07-20 17:39:48 +0200

    ref(note_item): preload notes when querying items

--- app/Services/todolist/FindItemsByUser.ts
+++ app/Services/todolist/FindItemsByUser.ts
@@ -3,6 +3,6 @@ import Item from 'App/Models/Item'
 
 export default class FindItemsByUser {
   public async invoke(auth: AuthContract) {
-    return Item.query().select().where('user_id', auth.user!.id)
+    return Item.query().preload('notes').select().where('user_id', auth.user!.id)
   }
 }
