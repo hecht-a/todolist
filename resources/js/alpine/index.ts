17bc8fdf06ec8a9174218b6b271b3a73c0265adb
HECHT Axel <axel.hct@gmail.com>
2022-07-20 17:28:58 +0200

    ref(note_item): export all alpine files in a single file (index.ts)

---
+++ resources/js/alpine/index.ts
@@ -0,0 +1,4 @@
+export { default as calendar } from './calendar'
+export { default as navbar } from './navbar'
+export { default as notes } from './notes'
+export { default as todolist } from './todolist'
--- resources/js/app.ts
+++ resources/js/app.ts
@@ -4,11 +4,8 @@ import dayjs from 'dayjs'
 import 'dayjs/locale/fr'
 
 import Alpine from 'alpinejs'
-import calendar from './alpine/calendar'
-import notes from './alpine/notes'
-import todolist from './alpine/todolist'
-import navbar from './alpine/navbar'
 import { Notyf } from 'notyf'
+import { calendar, navbar, notes, todolist } from './alpine'
 
 Alpine.data('todolist', todolist)
 Alpine.data('notes', notes)
@@ -24,12 +21,3 @@ window.notyf = new Notyf({
   duration: 2000,
   ripple: false,
 })
-
-// import { Calendar } from './Calendar'
-// const calendarElement = document.querySelector<HTMLDivElement>('#calendar')
-// if (calendarElement) {
-//   window.calendar = new Calendar(calendarElement, {
-//     min: new Date(),
-//     max: new Date(new Date().getFullYear() + Infinity, 10),
-//   })
-// }
